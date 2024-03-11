516211592dd0a19733949733a3abd815e887b6cb
swastik dayma <daymaswastik17@gmail.com>
2024-03-11 15:04:47 +0530

    js is here

---
+++ first.js
@@ -0,0 +1,295 @@
+// const prompt =require("prompt-sync")()
+
+// let age = prompt("enter your age: ")
+
+// if(age >= 18 && age<70  ){
+//     console.log("yes you can vote")
+// }
+// else if(age >=70){
+//  console.log("you are aged. please apne abaccho ke saath aao")
+// }
+// else{
+//     console.log("no you cannot vote")
+// }
+
+
+//    o	JS Strings methods
+// o	.length
+// o	.slice
+// o	.trim
+// o	.trimStart
+// o	.trimEnd
+// o	.replace
+// o	.replaceAll
+// o	.split
+// o	.charAt
+// o	.concat
+// o	ToUpperCase()
+// o	ToLowerCase
+// o	.includes
+
+
+
+
+// console.log(str.length) ;
+// console.log(strlen);
+
+// let upper= str.toLocaleUpperCase()
+// console.log(upper)
+
+
+// let lower= str.toLocaleLowerCase()
+// console.log(lower)
+// let length = str.length;
+// console.log(str)
+// console.log(length);
+
+// let trimmed = str.trim()
+// let trlen = trimmed.length;
+// console.log(trlen)
+// console.log(trimmed)
+
+// let sliced = str.slice(5, 15)
+// console.log(sliced)
+//  let str = "Quick brown Fox Jumps Over BROWN The Lazy Dog ";
+//  let str2 = "Quick brown Fox Jumps Over BROWN The Lazy Dog ";
+//  let str3 = "Quick brown Fox Jumps Over BROWN The Lazy Dog ";
+// let replaced= str.replaceAll(/brown/gi, "blue")
+// console.log(replaced)
+
+// let char  = str.charAt(3)
+// console.log(char)
+
+// let concat= str.concat(str2, str3)
+// console.log(concat)
+
+
+
+
+
+// o	.length
+// o	.toString
+// o	.pop
+// o	.push
+// o	.shift
+// o	.unshift
+// o	.join
+
+// o	.concat
+// o	.splice
+// o	.slice
+// o	.sort
+
+
+// const arry = [ "hello", "swastik"];
+
+// let joined = arry.sort()
+// console.log(joined)
+
+// let sorted = arry.sort();
+// console.log(sorted)
+
+// console.log(arry[6]);
+// arry[3]= "hello";
+
+// console.log(arry)
+
+// let spliced = arry.splice(2, 0 , "hello", "swastik" );
+// console.log(spliced)
+// console.log(arry)
+
+
+
+
+
+
+
+// let length = arry.length;
+// console.log(length)
+
+// let string  = arry.toString()
+// console.log(string)
+// console.log(arry)
+
+
+// let popped = arry.pop()
+// console.log(popped)
+// console.log(arry)
+
+// let pushed = arry.push("swastik", "hello")
+// console.log(pushed)
+// console.log(arry)
+
+// let shifted = arry.shift()
+// console.log(shifted)
+// console.log(arry)
+
+
+// console.log("swastik")
+// console.log("swastik")
+// console.log("swastik")
+// console.log("swastik")
+// console.log("swastik")
+// console.log("swastik")
+// console.log("swastik")
+// console.log("swastik")
+// console.log("swastik")
+// console.log("swastik")
+
+
+// for(let i = 5; i<=50; i= i+5 ){
+//     console.log(i)
+// }
+// const prompt = require('prompt-sync')()
+
+// let height = prompt("animal height? <1 / >=1 : ");
+
+// if(height < 1){
+//  let voice=    prompt("can squeak or not? yes / no:  ");
+//  if(voice == 'yes'){
+//     console.log("might be a rat")
+//  }
+//  else{
+//     console.log("might be a squirrel")
+//  }
+// }
+// else{
+//  let neck = prompt("neck size? short/ long  :  ");
+//  if(neck=="long"){
+//     console.log('might be giraffe')
+//  }else{
+//   let nose=   prompt("nose size? short / long : ");
+//   if(nose=="long"){
+//     console.log("elephant")
+//   }else{
+//   let live =   prompt("where does it live ? land/ water : ");
+//   if(live=="land"){
+//     console.log("Rhino")
+//   }else{
+//     console.log("hippo")
+//   }
+//   }
+//  }
+// }
+
+//while
+//dowhile
+//for each
+
+//map
+//filter
+//reduce
+
+
+// const arr = ["swastik", "shyam", "swapnil"];
+
+// for(let i = 0; i < arr.length; i++){
+//   console.log(arr[i])
+// }
+
+
+
+
+
+
+
+// for(let i = 1; i <= 10; i++){
+//   console.log(i)
+// }
+
+
+// let i = 1;
+
+// while(i <=10){
+//   console.log(i)
+//   i++
+// }
+
+
+// let x = 2;
+// do{
+//   console.log(x)
+//   x = x+2
+// }
+
+// while(x <=20)
+
+// let y = 3;
+
+// do{
+//   console.log(y)
+//   y=y+3
+// }
+// while(y<=30)
+
+
+
+
+
+
+
+// arr.map((value)=>{
+//   console.log(value*2)
+// })
+
+
+
+
+
+// const arr = [1, 2, 3, 4, 5];
+
+// let reduced = arr.reduce((prev, current)=>{
+//   return prev - current
+// })
+// console.log(reduced)
+// let arr2 = arr.filter((value)=>{
+//   return value==2
+//  })
+//  console.log(arr2)
+
+
+
+//  const arr3 = arr.reduce((prev, current)=>{
+// return prev + current
+// })
+// console.log(arr3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
